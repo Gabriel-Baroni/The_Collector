f23c29b391593ff365a61abd3e4155be921ae41d
Gabriel-Baroni <gdepaulabaroni@gmail.com>
2025-02-23 10:39:49 -0300

    Adicionando seguança API Key

---
+++ lib/backend/firebase/firebase_config.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+Future initFirebase() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: env['apiKey_firebase'],
+            authDomain: "the-collector-60b6e.firebaseapp.com",
+            projectId: "the-collector-60b6e",
+            storageBucket: "the-collector-60b6e.firebasestorage.app",
+            messagingSenderId: "944913016514",
+            appId: "1:944913016514:web:146407cb183832e8eabd5b",
+            measurementId: "G-YZF4Q3367V"));
+  } else {
+    await Firebase.initializeApp();
+  }
+}
